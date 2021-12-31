110b66d91fb6d04022529667aeb5fb4b42681fcd
Aron <AronKuch@gmail.com>
2021-12-31 07:39:19 -0500

    Through video 3.3, path and process (arguments)

--- FirstFile.js
+++ FirstFile.js
@@ -1,4 +1,13 @@
+const path = require("path");
+
+const grab = (flag) => {
+  return process.argv[process.argv.indexOf(flag) + 1];
+};
+
 let name = "Aron";
 let audience = "World";
 
 console.log(`Hello ${audience}! It's ${name}`);
+console.log(`The file name is ${path.basename(__filename)}`);
+console.log(`I'm running Node version ${process.versions.node}`);
+console.log(`Here's the user argument ${grab("--user")}`);
